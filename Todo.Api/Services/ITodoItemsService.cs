92cdca974898e9e524e3b8cb8ef4c39fa803b0ef
Nathan Dockens <ndocken@homeoffice.wal-mart.com>
2020-01-11 15:11:42 -0600

    Renamed solution and API project

---
+++ Todo.Api/Services/ITodoItemsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Api.Models;
+
+namespace Todo.Api.Services
+{
+    public interface ITodoItemsService
+    {
+        Task<IEnumerable<TodoItem>> GetTodoItemsAsync();
+        Task<TodoItem> GetTodoItemAsync(long id);
+        Task UpdateTodoItemAsync(long id, TodoItem todoItem);
+        Task<TodoItem> CreateTodoItemAsync(TodoItem todoItem);
+        Task<TodoItem> DeleteTodoItemAsync(long id);
+    }
+}
